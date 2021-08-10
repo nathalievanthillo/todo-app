219624e208fdcba7d1dafe2c4dbde169cad21f06
Nathalie Thillo <nathaliethillo@MacBook-Pro-van-Nathalie.local>
2021-08-10 15:36:31 +0200

    Logout

---
+++ logout.php
@@ -0,0 +1,5 @@
+<?php
+
+session_start(); //Session wordt gestart en kijkt of dat we zijn ingelogd
+session_destroy(); //Gooi de cookie weg
+header('location: login.php'); //je wordt teruggestuurd naar login.php zodat je terug kan inloggen
