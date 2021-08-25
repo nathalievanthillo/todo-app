57438ab3c2f9fca8dfa0ce087c2e068d39c1ce3f
nathalievanthillo <77227925+nathalievanthillo@users.noreply.github.com>
2021-08-26 01:02:35 +0200

    lists

--- classes/Lists.php
+++ classes/Lists.php
@@ -9,123 +9,137 @@ include_once(__DIR__ . "/Database.php");
         private $description;
         private $deadline;
 
-
-        public function getId()
-        {
-                return $this->Id;
+        public function getId(){
+            return $this->Id;
         }
 
 
-        public function setId($Id)
-        {
-                $this->Id = $Id;
+        public function setId($Id){
+            $this->Id = $Id;
         }
 
     
-        public function getUserId()
-        {
-                return $this->userId;
+        public function getUserId(){
+            return $this->userId;
         }
 
 
-        public function setUserId($userId)
-        {
-                $this->userId = $userId;
-        }
-
-      
-        public function getTitle()
-        {
-                return $this->title;
-        }
-
-        public function setTitle($title)
-        {
-                $this->title = $title;
+        public function setUserId($userId){
+            $this->userId = $userId;
         }
 
       
-        public function getDescription()
-        {
-                return $this->description;
+        public function getTitle(){
+            return $this->title;
+        }
+
+        public function setTitle($title){
+            $this->title = $title;
+        }
+
+      
+        public function getDescription(){
+            return $this->description;
         }
 
 
-        public function setDescription($description)
-        {
-                self::checkDescription($description);
-                $this->description = $description;
+        public function setDescription($description){
+            self::checkDescription($description);
+            $this->description = $description;
         }
 
 
         private static function checkDescription($description){
-                if($description == ""){
-                        throw new Exception("Description field cannot be empty");
-                }
+            if($description == ""){
+                    throw new Exception("Description field cannot be empty");
+            }
         }
 
         
         public static function getAllListsByUserId($userId){ //ALLE LIJSTEN OPVRAGEN VAN EEN SPECIFIEKE USER
-                $conn = Database::getConnection();
-                $query = $conn->prepare("SELECT id AS Id, user_id AS userId, title, description, deadline FROM lists WHERE user_id = :user");
+            $conn = Database::getConnection();
+            $query = $conn->prepare("SELECT id AS Id, user_id AS userId, title, description, deadline FROM lists WHERE user_id = :user");
 
-                $query->execute(['user' => $userId]);
-        
-                //return resultaat
-                $allLists = $query->fetchAll(PDO::FETCH_CLASS, "Lists" );
-                
-                return $allLists;
-            }
-
-            public function AddList(){
-                $conn = Database::getConnection();
-                $statement = $conn->prepare("INSERT INTO lists (user_id, title, description, deadline) VALUES (:userId, :title, :description, :deadline)");
+            $query->execute(['user' => $userId]);
+    
+            //return resultaat
+            $allLists = $query->fetchAll(PDO::FETCH_CLASS, "Lists" );
             
-                $userId     = $this->getUserId();
-                $title      = $this->getTitle();
-                $description  = $this->getDescription(); 
-                $deadline   = $this->getDeadline();
+            return $allLists;
+        }
+
+        public function AddList(){
+            $conn = Database::getConnection();
+            $statement = $conn->prepare("INSERT INTO lists (user_id, title, description, deadline) VALUES (:userId, :title, :description, :deadline)");
+        
+            $userId     = $this->getUserId();
+            $title      = $this->getTitle();
+            $description  = $this->getDescription(); 
+            $deadline   = $this->getDeadline();
+
+            $statement->bindValue(":userId", $userId);
+            $statement->bindValue(":title", $title);
+            $statement->bindValue(":description", $description);
+            $statement->bindValue(":deadline", $deadline);
 
 
-
-                $statement->bindValue(":userId", $userId);
-                $statement->bindValue(":title", $title);
-                $statement->bindValue(":description", $description);
-                $statement->bindValue(":deadline", $deadline);
-
-
-                $result = $statement->execute();
+            $result = $statement->execute();
 
             //return resultaat
             return $result;
+        }
+
+        public function getDeadline(){
+                return $this->deadline;
+            }
+    
+            public function setDeadline($deadline){
+                $this->deadline = $deadline;
             }
 
 
-            //delete je eigen lijst
-            public static function deleteLists($userId, $listId){
+            public static function getLists($user){
                 $conn = Database::getConnection();
-                $statement = $conn->prepare("DELETE FROM lists WHERE id = :listsId and user_id = :user");
-                
-                $statement->bindValue(":listsId", $listsId);
-                $statement->bindValue(":user", $userId);
-
-                $result = $statement->execute();
-
-                //return resultaat
+                $statement = $conn->prepare("SELECT * FROM lists WHERE user_id = :id");
+                $statement->bindValue(":id", $user->getUserId());
+                $statement->execute();
+                $result = $statement->fetchAll(\PDO::FETCH_OBJ);
                 return $result;
             }
 
-        /**
-         * Get the value of deadline
-         */ 
-        public function getDeadline()
-        {
-                return $this->deadline;
-        }
+            public static function countLists(){
+                $conn = Database::getConnection();
+                $statement = $conn->prepare("SELECT COUNT(*) FROM lists");
+    
+                $statement->execute();
+                $result = $statement->fetch(\PDO::FETCH_COLUMN);
+                return $result;
+            }
+            
+
+        //delete je eigen lijst
+       //delete je eigen lijst
+       public static function deleteLists($userId, $listId){
+        $conn = Database::getConnection();
+        $statement = $conn->prepare("DELETE FROM lists WHERE id = :listsId and user_id = :user");
+
+        $statement->bindValue(":listsId", $listId);
+        $statement->bindValue(":user", $userId);
+
+        $result = $statement->execute();
+
+        $conn = Database::getConnection();
+        $statement = $conn->prepare("DELETE FROM tasks WHERE list_id = :listId");
+
+        $statement->bindValue(":listId", $listId);
+
+        $result = $statement->execute();
+
+        //return resultaat
+        return $result;
+    }
 
 
-        public function setDeadline($deadline)
-        {
-                $this->deadline = $deadline;
-        }
+
+       
 }
