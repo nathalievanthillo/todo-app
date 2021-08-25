228e274e7e8e7cd27bc5da0ca316837787115b17
nathalievanthillo <77227925+nathalievanthillo@users.noreply.github.com>
2021-08-26 00:55:56 +0200

    index

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 include_once(__DIR__ . '/classes/Lists.php');
 include_once(__DIR__ . '/classes/Task.php');
 include_once(__DIR__ . '/classes/TaskComment.php');
+include_once(__DIR__ . '/classes/User.php');
 
     session_start(); //er wordt een link gelegd tussen session id en de data op de server
+
+
 
   $AllListsForUser[] = NULL; //declareren van de variable
   $AllTasksForListsAndUser[] = NULL;
@@ -15,14 +18,26 @@ include_once(__DIR__ . '/classes/TaskComment.php');
         //echo "Welcome " . $_SESSION['username'];
         //queries
 
+
+
         if(!isset($_GET['listId'])){ //wanneer we niet in een lijst zitten alle lijsten opvragen
-          $AllListsForUser = Lists::getAllListsByUserId($_SESSION['userId']); //alle lijsten tonen van de ingelogde user
+         
+            $AllListsForUser = Lists::getAllListsByUserId($_SESSION['userId']); //alle lijsten tonen van de ingelogde user
+          
+
+          
         } else {
+          
+
+          if(isset($_GET['order'])){
+
+            $AllTasksForListsAndUser = Task::getAllTasksByHours($_SESSION['userId'], $_GET['listId']);
+
+          }else{
+
             $AllTasksForListsAndUser = Task::getAllTasksByUserAndListId($_SESSION['userId'], $_GET['listId']); //user uit session halen en de lijst uit de get (url)
-
           }
-
-  
+          }
 
 
     }else{
@@ -58,8 +73,17 @@ include_once(__DIR__ . '/classes/TaskComment.php');
 
     </a>
     <a href="logout.php">Log out</a>
+
+    
   </nav>    
 </header>
+
+<?php if(User::IsAdmin($_SESSION['userId'])): ?> <!---Dit ziet enkel de admin deze 2 buttons---->
+<a class="headerbutton" href="createAdmin.php">Create new admin</a>
+
+<a class="headerbutton" href="statistics.php">Statistics</a>
+
+<?php endif;?>
 
 
 
@@ -68,28 +92,39 @@ include_once(__DIR__ . '/classes/TaskComment.php');
 <?php if(isset($_GET['listId'])): ?> 
   <a href="upload.php?listId=<?php echo $_GET['listId'];?>">New Task</a>
   <a href="index.php">Go Back</a>
+  
  
   <!---ALLE TAKEN DOORLOPEN---->
+  <a href="index.php?listId=<?php echo $_GET['listId'];?>&order=ascending">Sort by hours</a>
+  <?php $counter = 0;?>
   <?php foreach($AllTasksForListsAndUser as $Task){ ?> 
   <div class="task">
       <h3><?php echo $Task->getTitle();?></h3>
       <p><?php echo $Task->getHours();?></p>
       <p><?php echo $Task->getDeadline();?></p>
+      
+      <a href="deleteDeadlineTask.php?taskId=<?php echo $Task->getDeadline();?>">DELETE DEADLINE</a>
       <p class="status"><?php echo $Task->getStatus();?></p>
 
+
+ 
 
       <p>------</p>
       
       <!---ALLE COMMENTS VAN DE TAAK OVERLOPEN--->
       <div class="comments" data-taskid="<?php echo $Task->getTaskId() ?>" >
+     
+
       <?php foreach(TaskComment::getAllTaskCommentsByTaskId($Task->getTaskId()) as $TaskComment){ ?>
-        <p><?php echo $TaskComment->getComment();?></p>
+        <p><?php echo $TaskComment->getComment();?></p=>
+        
         <?php } ?>
       </div>
 
 <div class="commentBox">
         <input class="commentBoxText" type="text" name="comment">
-        <input class="commentBoxButton" type="button" name="submit" data-taskid="<?php echo $Task->getTaskId() ?>" value="submit">
+        <input class="commentBoxButton" type="button" name="submit" data-counter="<?php echo $counter?>" data-taskid="<?php echo $Task->getTaskId() ?>" value="submit">
+        <?php $counter = $counter+1?>
 
 </div>
 
@@ -100,7 +135,8 @@ include_once(__DIR__ . '/classes/TaskComment.php');
       <!---TaskId en listId meegeven met je URL--->
       <a href="deleteTask.php?taskId=<?php echo $Task->getTaskId();?>&listId=<?php echo $_GET['listId'];?>">Delete</a>
      
-    
+      
+
     </div>
     <?php } ?>
 
@@ -111,18 +147,36 @@ include_once(__DIR__ . '/classes/TaskComment.php');
 
  
 <!---LIJSTEN VAN VAN DE USER TONEN-------------------------------------------------------------------> 
+
+
+
+<div class="fullList">
 <h2>Alle lijsten</h2>
+<div class="AllList">
+<div class="newList">
 <a href="addlist.php">Add List</a>
+</div>
+</div>
+      </div>
+
 
 
 <?php foreach($AllListsForUser as $ListForUser){ ?>
-  <div class="test">
+  <div class="infoList">
+    <div class="dataList">
+
       <a href="index.php?listId=<?php echo $ListForUser->getId();?>">
+      
         <h3><?php echo $ListForUser->getTitle();?></h3>
+        <p><?php echo $ListForUser->getDeadline();?></p>
       </a>
       <p><?php echo $ListForUser->getDescription();?></p>
+      
 
-      <a href="deleteLists.php?listId=<?php echo $Lists->getTaskId();?>&listId=<?php echo $_GET['listId'];?>">Delete</a>
+      
+
+      <a href="deleteLists.php?listId=<?php echo $ListForUser->getId();?>">DELETE LIST</a>
+      </div>
     </div>
     
     <?php } ?>>
