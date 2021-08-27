353f3825d0d7e5ec4dbc244a85513772d9bff2f7
nathalievanthillo <77227925+nathalievanthillo@users.noreply.github.com>
2021-08-27 02:14:31 +0200

    create admin

--- createAdmin.php
+++ createAdmin.php
@@ -2,7 +2,7 @@
     include_once(__DIR__ . "/classes/User.php");
 
 
-    if(!empty($_POST)){ //zodra de post niet leeg is weten we dat er iets verzonden is en laten we de variables uitlezen
+    if(!empty($_POST)){ 
 
 
     
@@ -38,9 +38,7 @@
 
 <div id="app">
 
-<!--form action kan je ook leeg laten dan is het de huidige pagina-->
-<!--bij een get methode komt de data in de URL te staan = onveilig, bij zoekformulieren wel van toepassing-->
-<!--Post is een super global-->
+
     <form action="createAdmin.php" method="post">
     <div class="logo">
           <img src="images/logo-app.png">
@@ -53,7 +51,6 @@
   
 
     <?php if(isset($error)): ?>
-    <!--Bestaat er een variable met de naam error dan gaan we onderstaand blok uitvoeren & tonen we de foutmelding-->
     <div class="alert">
       <?php echo $error;?></div>
     <?php endif; ?>
@@ -65,7 +62,6 @@
 
   <div class="form form--login">
     <label for="username">Username</label>
-    <!--Inputvelden moeten een id hebben maar ook een name attribuut krijgen anders wordt er niets doorgestuurd naar de backend -->
     <input type="text" id="username" name="username">
 
 
@@ -73,7 +69,6 @@
     <input type="text" id="email" name="email">
   
     <label for="password">Password</label>
-    <!--Inputvelden moeten een id hebben maar ook een name attribuut krijgen anders wordt er niets doorgestuurd naar de backend -->
     <input type="password" id="password" name="password">
   </div>
   
