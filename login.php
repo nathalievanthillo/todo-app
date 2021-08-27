fce009f96e4a358dbc486951c3f6c04661214a2b
nathalievanthillo <77227925+nathalievanthillo@users.noreply.github.com>
2021-08-27 02:12:52 +0200

    log in

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ if(!empty($_POST)){
     $password = $_POST['password'];
 
     if(User::canLogin($username, $password)){ 
-        session_start(); //onthouden dat de gebruiker is ingelogd
+        session_start(); 
 
         $_SESSION["username"] = $username;
         $_SESSION["userId"] = User::getUserIdByName($username);
@@ -35,9 +35,7 @@ if(!empty($_POST)){
     
 <div id="app">
 
-<!--form action kan je ook leeg laten dan is het de huidige pagina-->
-<!--bij een get methode komt de data in de URL te staan = onveilig, bij zoekformulieren wel van toepassing-->
-<!--Post is een super global-->
+
     <form action="login.php" method="post">
       <div class="logo">
           <img src="images/logo-app.png">
@@ -49,21 +47,21 @@ if(!empty($_POST)){
     </nav>
   
 
-    <!--is een variable geset met de naam error, staat er een naam met de naam error-->
+
     <?php if(isset($error)): ?>
-    <!--Bestaat er een variable met de naam error dan gaan we onderstaand blok uitvoeren & tonen we de foutmelding-->
-    <div class="alert">That password was incorrect. Please try again</div>
+    <div class="alert">Username or password are incorrect.</div>
     <?php endif; ?>
+
+
+
 
 
 
   <div class="form form--login">
     <label for="username">Username</label>
-    <!--Inputvelden moeten een id hebben maar ook een name attribuut krijgen anders wordt er niets doorgestuurd naar de backend -->
     <input type="text" id="username" name="username">
   
     <label for="password">Password</label>
-    <!--Inputvelden moeten een id hebben maar ook een name attribuut krijgen anders wordt er niets doorgestuurd naar de backend -->
     <input type="password" id="password" name="password">
   </div>
   
