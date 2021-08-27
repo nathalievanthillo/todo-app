41084c99874662cde0f9bfcd3ab8704e42bbb910
nathalievanthillo <77227925+nathalievanthillo@users.noreply.github.com>
2021-08-27 02:12:19 +0200

    register

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
     include_once(__DIR__ . "/classes/User.php");
 
 
-    if(!empty($_POST)){ //zodra de post niet leeg is weten we dat er iets verzonden is en laten we de variables uitlezen
+    if(!empty($_POST)){ 
 
 
         try{
@@ -35,9 +35,7 @@
 
 <div id="app">
 
-<!--form action kan je ook leeg laten dan is het de huidige pagina-->
-<!--bij een get methode komt de data in de URL te staan = onveilig, bij zoekformulieren wel van toepassing-->
-<!--Post is een super global-->
+
     <form action="register.php" method="post">
     <div class="logo">
           <img src="images/logo-app.png">
@@ -50,7 +48,6 @@
   
 
     <?php if(isset($error)): ?>
-    <!--Bestaat er een variable met de naam error dan gaan we onderstaand blok uitvoeren & tonen we de foutmelding-->
     <div class="alert">
       <?php echo $error;?></div>
     <?php endif; ?>
@@ -62,7 +59,6 @@
 
   <div class="form form--login">
     <label for="username">Username</label>
-    <!--Inputvelden moeten een id hebben maar ook een name attribuut krijgen anders wordt er niets doorgestuurd naar de backend -->
     <input type="text" id="username" name="username">
 
 
@@ -70,7 +66,6 @@
     <input type="text" id="email" name="email">
   
     <label for="password">Password</label>
-    <!--Inputvelden moeten een id hebben maar ook een name attribuut krijgen anders wordt er niets doorgestuurd naar de backend -->
     <input type="password" id="password" name="password">
   </div>
   
