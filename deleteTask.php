e36c7645f84d27d08974658797cc0b1732cb8db6
nathalievanthillo <77227925+nathalievanthillo@users.noreply.github.com>
2021-08-27 02:13:37 +0200

    delete task

--- deleteTask.php
+++ deleteTask.php
@@ -2,10 +2,10 @@
 
 include_once(__DIR__ . '/classes/Task.php');
 
-session_start(); //Session wordt gestart en kijkt of dat we zijn ingelogd
+session_start(); 
 
-Task::deleteTask($_SESSION['userId'], $_GET['taskId']); //userId komt uit session en kan alleen verkregen worden door een login
+Task::deleteTask($_SESSION['userId'], $_GET['taskId']);
 
-header("location:index.php?listId=".$_GET['listId']); //je wordt teruggestuurd naar de pagina van de lijst
+header("location:index.php?listId=".$_GET['listId']);
 
 ?>
