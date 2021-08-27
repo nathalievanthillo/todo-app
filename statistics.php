194a7bc2a683a6a8cb0f4e4dd5632875b54c733d
nathalievanthillo <77227925+nathalievanthillo@users.noreply.github.com>
2021-08-27 02:11:46 +0200

    statistics

--- statistics.php
+++ statistics.php
@@ -4,7 +4,7 @@ include_once(__DIR__ . '/classes/Database.php');
 include_once(__DIR__ . '/classes/Lists.php');
 include_once(__DIR__ . '/classes/Task.php');
 
-session_start(); //er wordt een link gelegd tussen session id en de data op de server
+session_start(); 
 
 $user = new User();
 $list = new Lists();
@@ -35,11 +35,9 @@ $users = $user->getUsers($user);
       <div class="user--avatar"><img src="images/image-user.png" alt=""></div>
   
       <span class="user--status">Welcome</span>
-      <!--als er geset is uit de session vakje username-->
       <?php if(isset($_SESSION['username'])): ?> 
-      <!--doen we onderstaand blok gaan we de username vanuit de session printen-->
-      <h3 class="user--name"><?php echo $_SESSION['username']; ?></h3>
-      <?php else: ?> <!--als die sessie niet bestaat wordt username here afgeprint-->
+      <h3 class="user--name"><?php echo (htmlspecialchars($_SESSION['username'])); ?></h3>
+      <?php else: ?> 
       <h3 class="user--name">Username here</h3>
       <?php endif; ?>
 
@@ -49,12 +47,26 @@ $users = $user->getUsers($user);
     
   </nav>    
 </header>
+<div class="navigationStatics">
+<a href="index.php">Go Back</a>
+</div>
+<div class="allStats">
+<h2>Statistics</h2>
+</div>
 
-<p class="stats">Er zijn <?php echo $countusers; ?> users</p>
-<p class="stats">Er zijn <?php echo $countlists; ?> lists</p>
+<div class="infoList">
+    <div class="dataList">
+<div class="userStats">
+<h3>Users</h3>
+<p class="statsUser">There are <b><?php echo $countusers; ?></b> users</p>
+</div>
 
-
-<p>test</p>
+<div class="listStats">
+<h3>Lists</h3>
+<p class="statsLists">There are <b><?php echo $countlists; ?></b> lists</p>
+</div>
+</div>
+</div>
 </body>
 
 </html>
