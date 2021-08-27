db0b013be3db3d1e55d6c046bbd1a1c6d0def221
nathalievanthillo <77227925+nathalievanthillo@users.noreply.github.com>
2021-08-27 02:13:56 +0200

    delete lists

--- deleteLists.php
+++ deleteLists.php
@@ -2,10 +2,9 @@
 
 include_once(__DIR__ . '/classes/Lists.php');
 
-session_start(); //Session wordt gestart en kijkt of dat we zijn ingelogd
+session_start(); 
 
-$result = Lists::deleteLists($_SESSION['userId'], $_GET['listId']); //userId komt uit session en kan alleen verkregen worden door een login
-
-header("location:index.php"); //je wordt teruggestuurd naar de pagina van de lijst
+$result = Lists::deleteLists($_SESSION['userId'], $_GET['listId']); 
+header("location:index.php");
 ?>
 
