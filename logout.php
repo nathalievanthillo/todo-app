8975bfcc83176f2d2a8f4ebd89dc37144c07c533
nathalievanthillo <77227925+nathalievanthillo@users.noreply.github.com>
2021-08-27 02:12:35 +0200

    logout

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 
-session_start(); //Session wordt gestart en kijkt of dat we zijn ingelogd
-session_destroy(); //Gooi de cookie weg
-header("location:login.php"); //je wordt teruggestuurd naar login.php zodat je terug kan inloggen
+session_start(); 
+session_destroy(); 
+header("location:login.php"); 
 
 ?>
